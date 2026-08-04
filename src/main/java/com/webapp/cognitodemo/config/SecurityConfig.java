c0c38a418b3a005da3567b62d625f2175ebe56a8
pratham <cprathamesh199@gmail.com>
2026-08-04 11:53:57 +0530

    Allowed origins changed

--- src/main/java/com/webapp/cognitodemo/config/SecurityConfig.java
+++ src/main/java/com/webapp/cognitodemo/config/SecurityConfig.java
@@ -104,7 +104,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:5173",
-
+                "https://api.cyfenix.com",
                 "https://studentportal.cyfenix.com"
         ));
 
