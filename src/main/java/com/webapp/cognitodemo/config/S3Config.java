71ecf85dc02ecb4c5a6df3b38f20c8a6419f7f2f
pratham <cprathamesh199@gmail.com>
2026-09-17 16:47:35 +0530

    Changed the TPOAdmin page

--- src/main/java/com/webapp/cognitodemo/config/S3Config.java
+++ src/main/java/com/webapp/cognitodemo/config/S3Config.java
@@ -1,73 +1,4 @@
-package com.webapp.cognitodemo.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3Configuration;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-
-import java.net.URI;
-
-/*
- * Points the app's S3Client/S3Presigner at Zata.AI's S3-compatible
- * object storage instead of AWS S3. Zata is used as a drop-in
- * replacement via the AWS SDK's endpoint override + path-style access.
- */
-@Configuration
-public class S3Config {
-
-    @Value("${zata.accessKey}")
-    private String accessKey;
-
-    @Value("${zata.secretKey}")
-    private String secretKey;
-
-    @Value("${zata.s3.region}")
-    private String region;
-
-    @Value("${zata.s3.endpoint}")
-    private String endpoint;
-
-    // Zata requires path-style access (https://endpoint/bucket/key)
-    @Value("${zata.s3.pathStyleAccess:true}")
-    private boolean pathStyleAccess;
-
-    @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)))
-                .serviceConfiguration(S3Configuration.builder()
-                        .pathStyleAccessEnabled(pathStyleAccess)
-                        .build())
-                .endpointOverride(URI.create(endpoint))
-                .build();
-    }
-
-    @Bean
-    public S3Presigner s3Presigner() {
-        return S3Presigner.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)))
-                .serviceConfiguration(S3Configuration.builder()
-                        .pathStyleAccessEnabled(pathStyleAccess)
-                        .build())
-                .endpointOverride(URI.create(endpoint))
-                .build();
-    }
-}
-
-
-
-
-
-
+//ZATA CODE below
 
 //package com.webapp.cognitodemo.config;
 //
@@ -78,19 +9,34 @@ public class S3Config {
 //import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 //import software.amazon.awssdk.regions.Region;
 //import software.amazon.awssdk.services.s3.S3Client;
+//import software.amazon.awssdk.services.s3.S3Configuration;
 //import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 //
+//import java.net.URI;
+//
+///*
+// * Points the app's S3Client/S3Presigner at Zata.AI's S3-compatible
+// * object storage instead of AWS S3. Zata is used as a drop-in
+// * replacement via the AWS SDK's endpoint override + path-style access.
+// */
 //@Configuration
 //public class S3Config {
 //
-//    @Value("${aws.accessKey}")
+//    @Value("${zata.accessKey}")
 //    private String accessKey;
 //
-//    @Value("${aws.secretKey}")
+//    @Value("${zata.secretKey}")
 //    private String secretKey;
 //
-//    @Value("${aws.s3.region}")
+//    @Value("${zata.s3.region}")
 //    private String region;
+//
+//    @Value("${zata.s3.endpoint}")
+//    private String endpoint;
+//
+//    // Zata requires path-style access (https://endpoint/bucket/key)
+//    @Value("${zata.s3.pathStyleAccess:true}")
+//    private boolean pathStyleAccess;
 //
 //    @Bean
 //    public S3Client s3Client() {
@@ -98,6 +44,10 @@ public class S3Config {
 //                .region(Region.of(region))
 //                .credentialsProvider(StaticCredentialsProvider.create(
 //                        AwsBasicCredentials.create(accessKey, secretKey)))
+//                .serviceConfiguration(S3Configuration.builder()
+//                        .pathStyleAccessEnabled(pathStyleAccess)
+//                        .build())
+//                .endpointOverride(URI.create(endpoint))
 //                .build();
 //    }
 //
@@ -107,9 +57,61 @@ public class S3Config {
 //                .region(Region.of(region))
 //                .credentialsProvider(StaticCredentialsProvider.create(
 //                        AwsBasicCredentials.create(accessKey, secretKey)))
+//                .serviceConfiguration(S3Configuration.builder()
+//                        .pathStyleAccessEnabled(pathStyleAccess)
+//                        .build())
+//                .endpointOverride(URI.create(endpoint))
 //                .build();
 //    }
 //}
+//
+
+
+//ZATA CODE ABOVE
+
+
+
+package com.webapp.cognitodemo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+}
 
 
 
